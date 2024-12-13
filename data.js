7a01e3c9f9e58da4b755abed785045dc94d2bc05
Dath Analytics <dath.analyst@gmail.com>
2024-12-13 04:15:20 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,34 @@
 const links = [
-  { title: "Portfolio", url: "https://your-personal-portfolio.com", customIcon: "images/logo.png" },
-  { title: "LinkedIn", url: "https://linkedin.com/in/your-profile", icon: "fab fa-linkedin" },
-  { title: "GitHub", url: "https://github.com/daththeanalyst", icon: "fab fa-github" },
-  { title: "Spotify", url: "https://open.spotify.com/user/your-profile", icon: "fab fa-spotify" },
-  { title: "Instagram", url: "https://instagram.com/your-profile", icon: "fab fa-instagram" },
-  { title: "Contact Me", url: "mailto:your-email@example.com", icon: "fas fa-envelope" },
+  { 
+    title: "Portfolio", 
+    url: "https://daththeanalyst.github.io/Portfolio-Dath/", 
+    customIcon: "images/logo.png" 
+  },
+  { 
+    title: "LinkedIn", 
+    url: "https://linkedin.com/in/your-profile", 
+    icon: "fab fa-linkedin" 
+  },
+  { 
+    title: "GitHub", 
+    url: "https://github.com/daththeanalyst", 
+    icon: "fab fa-github" 
+  },
+  { 
+    title: "Spotify", 
+    url: "https://open.spotify.com/user/21kur5ehx2xx5hwxnv4bmdpxq?si=0c15eaf71b204a16", 
+    icon: "fab fa-spotify" 
+  },
+  { 
+    title: "Instagram", 
+    url: "https://www.instagram.com/dim.ath_/", 
+    icon: "fab fa-instagram" 
+  },
+  { 
+    title: "Contact Me", 
+    url: "mailto:dath.analyst@gmail.com", 
+    icon: "fas fa-envelope" 
+  },
 ];
+
 export default links;
