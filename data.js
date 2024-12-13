064356a06bcbe6879af16e7d8df7584bc542a559
Dath Analytics <dath.analyst@gmail.com>
2024-12-13 03:34:38 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const links = [
+  { title: "GitHub", url: "https://github.com/daththeanalyst" },
+  { title: "LinkedIn", url: "https://linkedin.com/in/your-profile" },
+  { title: "Twitter", url: "https://twitter.com/your-profile" },
+  { title: "Email Me", url: "mailto:your-email@example.com" },
+];
+
+// Export the links so index.js can use them
+export default links;
