fdd3bc97fe4b5beb72da57195b867d96854bc0dd
Dath Analytics <dath.analyst@gmail.com>
2024-12-13 04:05:51 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 const links = [
-  { title: "GitHub", url: "https://github.com/daththeanalyst", icon: "fab fa-github" },
-  { title: "LinkedIn", url: "https://linkedin.com/in/your-profile", icon: "fab fa-linkedin" },
-  { title: "Instagram", url: "https://instagram.com/your-profile", icon: "fab fa-instagram" },
-  { title: "Spotify", url: "https://open.spotify.com/user/your-profile", icon: "fab fa-spotify" },
   { title: "Portfolio", url: "https://your-personal-portfolio.com", customIcon: "images/logo.png" },
+  { title: "LinkedIn", url: "https://linkedin.com/in/your-profile", icon: "fab fa-linkedin" },
+  { title: "GitHub", url: "https://github.com/daththeanalyst", icon: "fab fa-github" },
+  { title: "Spotify", url: "https://open.spotify.com/user/your-profile", icon: "fab fa-spotify" },
+  { title: "Instagram", url: "https://instagram.com/your-profile", icon: "fab fa-instagram" },
+  { title: "Contact Me", url: "mailto:your-email@example.com", icon: "fas fa-envelope" },
 ];
 
 export default links;
